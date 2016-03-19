a01bce68e616aa634f516b1f47499c9844b6a714
Samuel Georges <sam@daftspunk.com>
2016-03-19 13:21:24 +1100

    Access the title from the viewBag array

--- components/StaticPage.php
+++ components/StaticPage.php
@@ -53,7 +53,7 @@ class StaticPage extends ComponentBase
         $this->pageObject = $this->page['page'] = $router->findByUrl($url);
 
         if ($this->pageObject) {
-            $this->title = $this->page['title'] = $this->pageObject->getViewBag()->property('title');
+            $this->title = $this->page['title'] = array_get($this->pageObject->viewBag, 'title');
             $this->extraData = $this->page['extraData'] = $this->defineExtraData();
         }
     }
