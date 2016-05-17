8d10575c63980097912d973229353f57dff3e7e3
Samuel Georges <sam@daftspunk.com>
2016-05-18 05:00:22 +1000

    Side panel should be fixed by default

--- controllers/Index.php
+++ controllers/Index.php
@@ -82,7 +82,7 @@ class Index extends Controller
         // before it loads dynamically.
         $this->addJs('/modules/backend/formwidgets/codeeditor/assets/js/build-min.js', 'core');
 
-        $this->bodyClass = 'compact-container side-panel-not-fixed';
+        $this->bodyClass = 'compact-container';
         $this->pageTitle = 'rainlab.pages::lang.plugin.name';
         $this->pageTitleTemplate = '%s Pages';
 
